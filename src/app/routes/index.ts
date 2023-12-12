a13729317cf743f4d69a0768aec5496ca599ff67
Nasim <nasimcse16@gmail.com>
2023-12-13 01:16:29 +0600

    ad academic faculty and department

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,18 +1,28 @@
 import { Router } from "express";
-import userRoutes from "../modules/users/user.route";
-import studentRoutes from "../modules/student/student.route";
+import { UserRoutes } from "../modules/users/user.route";
+import { StudentRoutes } from "../modules/student/student.route";
+import { AcademicFacultyRoutes } from "../modules/academicFaculty/academicFaculty.route";
+import { AcademicDepartmentRoutes } from "../modules/academicDepartment/academicDepartment.route";
 
 const router = Router();
 
 const moduleRoutes = [
     {
         path: '/users',
-        route: userRoutes,
+        route: UserRoutes,
     },
     {
         path: '/students',
-        route: studentRoutes,
-    }
+        route: StudentRoutes,
+    },
+    {
+        path: '/academic-faculties',
+        route: AcademicFacultyRoutes,
+    },
+    {
+        path: '/academic-departments',
+        route: AcademicDepartmentRoutes,
+    },
 ]
 
 moduleRoutes.forEach((route) => { router.use(route.path, route.route) })
